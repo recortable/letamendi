182a16eea6314cf8ce6cfe5b2fa5f187ac33fcbe
dani <dani@mandarine.(none)>
2008-12-09 17:44:29 +0100

    will paginate no longed needed

--- config/environment.rb
+++ config/environment.rb
@@ -28,8 +28,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
-  config.gem 'mislav-will_paginate', :version => '~> 2.2.3', :lib => 'will_paginate',
-    :source => 'http://gems.github.com'
+  #config.gem 'mislav-will_paginate', :version => '~> 2.2.3', :lib => 'will_paginate', :source => 'http://gems.github.com'
 
 
   # Only load the plugins named here, in the order given. By default, all plugins 
